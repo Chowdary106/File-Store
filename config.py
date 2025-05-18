5cc4558290cafae8ea7d9384648c9575550a9720
Chowdary <131300937+Chowdary106@users.noreply.github.com>
2025-05-18 21:20:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "colorful-jerry-chowdary106-900bc6ac.koyeb.app/")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
